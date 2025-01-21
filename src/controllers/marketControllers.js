97b52248f2571b5d62c9a0cc8690ee0b96883a3a
Nicola31082005 <nikule512@gmail.com>
2025-01-21 22:02:19 +0200

    add Pizza details page

--- src/controllers/marketControllers.js
+++ src/controllers/marketControllers.js
@@ -10,6 +10,16 @@ marketController.get('/pizzas', (req, res) => {
     res.render('pizzas', { title: 'Pizzas', pizzas: pizzas })
 })
 
+marketController.get('/cart/add/:id', (req, res) => {
+    const currentId = Number(req.params.id);
+    const currentPizza = getAllPizzas().find(pizzaObj => pizzaObj.id === currentId)
+
+    res.render('details', { title: "Details", pizza: currentPizza })
+    
+
+
+})
+
 
 
 export default marketController
